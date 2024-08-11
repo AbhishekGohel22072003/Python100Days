a29339067d7df17b0c195f0ed1ca33190c7e9f9e
AbhishekGohel22072003 <apgohel98@gmail.com>
2024-08-12 00:24:46 +0530

    Changes upto 8_3

--- Day 8/8_3_Paint_Estimation.py
+++ Day 8/8_3_Paint_Estimation.py
@@ -1,6 +1,16 @@
 # to paint 5 square meters of we need 1 can of paint
-
+print("To get estimate of the ")
 height = float(input("Enter the height of the wall in meters: "))
 width = float(input("Enter the width of the wall in meters: "))
 
+print(f'The total area of wall is {height*width} square meters.')
 
+no_of_cans = (height*width)/5
+rounded_no_of_cans = 0
+if no_of_cans > int(no_of_cans):
+    rounded_no_of_cans = int(no_of_cans+1)
+else:
+    rounded_no_of_cans = int(no_of_cans)
+
+
+print(f"You will need approx. {rounded_no_of_cans} to paint the wall.")
