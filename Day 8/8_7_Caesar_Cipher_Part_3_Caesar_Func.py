ce40d8e5a45deade3325a3f7b71359cd95b7fa8a
AbhishekGohel22072003 <apgohel98@gmail.com>
2024-08-13 00:42:50 +0530

    Done chanes and bug fixes in 8_7

--- Day 8/8_7_Caesar_Cipher_Part_3_Caesar_Func.py
+++ Day 8/8_7_Caesar_Cipher_Part_3_Caesar_Func.py
@@ -1,25 +1,51 @@
 #TODO: Create a common function called 'caesar' that takes the 'text', 'shift' and 'direction' as inputs and gives the output directly by calling that function.
 #      Also it gets rid of encrypt() and decrypt() functions
+import sys
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-if direction not in ("encode","decode"):
-    print("You have entered wrong value.")
-    exit
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))  
+
 
 def caesar(text, shift, direction):        
     text_list = list(text)
+    
+    # TODO: What if the entered text contains the number or special characters?
+    # Ans: To ena mate aapne niche if text[i] in alphabet vali condition add kari chhe
+    
     for i in range(len(text)):
-        position = alphabet.index(text_list[i])
-        if direction == "encode":
-            new_position = (position + shift) % len(alphabet)
-        elif direction == 'decode':
-            new_position = (position - shift) % len(alphabet)
-        text_list[i] = alphabet[new_position]
-        plain_text = ''.join(text_list)
+        if text[i] in alphabet:
+            position = alphabet.index(text_list[i])
+            if direction == "encode":
+                new_position = (position + shift) % len(alphabet)
+            elif direction == 'decode':
+                new_position = (position - shift) % len(alphabet)
+            text_list[i] = alphabet[new_position]
+            plain_text = ''.join(text_list)
+        elif text[i] not in alphabet:
+            plain_text = ''.join(text_list)
     print(f'The {direction}d text is {plain_text}')
 
-caesar(text=text, shift=shift,direction=direction)
+
+
+# TODO: Ask the user to ask if they want to restart the cipher program?
+# Type 'YES' if you want to go again else 'NO'
+
+
+
+def yes_or_no():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction not in ("encode","decode"):
+        print("You have entered wrong value.")
+        sys.exit()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))  
+
+
+    caesar(text=text, shift=shift,direction=direction)
+
+    y_or_n = input("Type 'yes' if you want to go again. Otherwise type 'no'.: ") 
+    if y_or_n == 'yes':
+        yes_or_no()
+        
+        
+yes_or_no()
