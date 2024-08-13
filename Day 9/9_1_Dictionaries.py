6e1c608d8547f0fc8e7c4255f2f051347da2ee90
AbhishekGohel22072003 <apgohel98@gmail.com>
2024-08-13 16:42:45 +0530

    Added &completed 9_1 and 9_2 in Day 9

---
+++ Day 9/9_1_Dictionaries.py
@@ -0,0 +1,69 @@
+# Dictionaries are mutable
+# Dictionaries can contain the different type of values
+
+
+
+
+'''Create'''
+# Creating a dictionary
+dictionary = {
+    "Abhishek" : "Gohel",
+    "Adarsh" : "Borisagar",
+    "Shyam" : "Tandel",
+    "" : "", #i.e. we can do this also
+    "End of dict" : "ending",
+}
+
+
+# We can create it another way also using dict()
+dictionary_using_dict_constructor = dict(
+    Abhishek="Gohel",
+    Adarsh='Borisagar',
+    Kunal="Balchandani",
+    # ="",  We cannot generate the empty value in this dict contructor
+)
+
+
+
+
+
+'''Read'''
+# Reading The elements of the dictionary
+print(f"IF we do print(dictionary) then: {dictionary}")  # IF we do print(dictionary) then: {'Abhishek': 'Gohel', 'Adarsh': 'Borisagar', 'Shyam': 'Tandel', '': '', 'End of dict': 'ending'}
+
+# Printing specific index
+print(f"IF we do print(dictionary['Abhishek']) then: {dictionary['Abhishek']}") # IF we do print(dictionary['Abhishek']) then: Gohel
+
+# What if we use for loop
+for i in dictionary:
+    print(i)
+    #This will print the following:
+    # Abhishek
+    # Adarsh
+    # Shyam
+
+    # End of dict
+
+for i in dictionary:
+    print(dictionary[i])
+    #This will print the following:
+    # Gohel
+    # Borisagar
+    # Tandel
+
+    # ending
+
+
+'''Update'''
+
+# Adding The value in the dictionary
+dictionary["New value"] = "upadating now"
+
+print(dictionary) # {'Abhishek': 'Gohel', 'Adarsh': 'Borisagar', 'Shyam': 'Tandel', '': '', 'End of dict': 'ending', 'New value': 'upadating now'}
+
+
+'''Delete'''
+
+# Deleting the values from the dictionary
+dictionary = {}
+print("After wiping all the elements from the dictionary \ni.e. dictionary = {} when we do print(dictionary) then we get:",dictionary)
---
+++ Day 9/9_2_Grading_Program_using_dictionaries.py
@@ -0,0 +1,33 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# if marks between 91-100 then grade:"Outstanding"
+# if marks between 81-90 then grade:"Exceeds Expectations"
+# if marks between 71-80 then grade:"Acceptable"
+# if marks are 70 or lower then grade:"Fail"
+
+
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for i in student_scores:
+    if student_scores[i] in range(91,101):
+        student_grades[i] = "Outstanding"
+    elif student_scores[i] in range(81,91):
+        student_grades[i] = "Exceeds Expectations"
+    elif student_scores[i] in range(71,81):
+        student_grades[i] = "Acceptable"
+    elif student_scores[i] in range(0,71):
+        student_grades[i] = "Fail"
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
