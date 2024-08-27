fde6d7e29af486ab15408e29a803fc442e264bc6
AbhishekGohel22072003 <apgohel98@gmail.com>
2024-08-28 00:17:08 +0530

    Completed Day 13

---
+++ Day 13/13_1_Debugging_Tips.py
@@ -0,0 +1,19 @@
+# 1: Describe the problem
+
+# 2: Reproduce the bug (to see that where we are getting a bug)
+
+# 3: Play computer (i.e. analyse code line by line as you are a computer)
+
+# 4: Fix the errors
+
+# 5: Print() is your friend
+
+# 6: Use a debugger available by python by searching "python debugger"
+
+# 7: Take a Break
+
+# 8: Ask a friend(probably a developer)
+
+# 9: To run your code often and try to fix all the bugs at a time
+
+# 10: Ask stack overflow
