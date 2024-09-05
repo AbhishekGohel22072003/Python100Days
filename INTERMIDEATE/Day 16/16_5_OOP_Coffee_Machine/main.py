0c090ffdecb10a2d2ac9d45735e860fc9f027b84
AbhishekGohel22072003 <apgohel98@gmail.com>
2024-09-05 09:39:33 +0530

    Changes in main.py of 16_5

--- INTERMIDEATE/Day 16/16_5_OOP_Coffee_Machine/main.py
+++ INTERMIDEATE/Day 16/16_5_OOP_Coffee_Machine/main.py
@@ -20,6 +20,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        print(drink)
+        # print(drink)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
             coffee_maker.make_coffee(drink)
